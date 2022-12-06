76b30dce5cf478e7cea4361d28cfc6c87770ddef
Rachana Kumbhakar <rachanakumbhakar2019@gmail.com>
2022-12-06 10:31:24 -0700

    color

--- Magic_Piano_Tiles/Tilescolor.cs
+++ Magic_Piano_Tiles/Tilescolor.cs
@@ -5,7 +5,7 @@ using System.Numerics;
 class ObjectColor {
    protected Random random = new Random();
    protected List<Color> ColorsList = new List<Color>();
-   protected Color[] colors = { Color.LIGHTGRAY, Color.GRAY, Color.DARKGRAY, Color.YELLOW, Color.GOLD, Color.ORANGE, Color.PINK, Color.RED, Color.MAROON, Color.GREEN, Color.LIME, Color.DARKGREEN, Color.SKYBLUE, Color.BLUE, Color.DARKBLUE, Color.PURPLE, Color.VIOLET, Color.DARKPURPLE, Color.BEIGE, Color.BROWN, Color.DARKBROWN};
+   protected Color[] colors = { Color.WHITE, Color.BLACK};
 
    protected Color backgroundColor;
    protected Color playerColor;
